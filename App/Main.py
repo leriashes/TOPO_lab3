81f05eaaef96c893a55688d104f3e780894d9b05
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-03-29 19:33:13 +0700

    Отрисовка окна

--- App/Main.py
+++ App/Main.py
@@ -1,9 +1,27 @@
+import pygame as pg
 from App.Grid import Grid
+
+WIDTH, HEIGHT = 640, 640
+BG_COLOR = (229, 223, 189)
 
 def main():
     grid = Grid()
 
     grid.shuffleCells()
+
+    pg.init()
+    pg.display.set_caption("Пятнашки")
+
+    window = pg.display.set_mode((WIDTH, HEIGHT))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+
+        window.fill(BG_COLOR)
+
+        pg.display.update()
  
 if __name__ == "__main__":
     main()
