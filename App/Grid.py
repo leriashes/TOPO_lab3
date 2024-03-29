3a24deef46a1c0c813a4eaa254f8d0571d0befb0
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-03-29 17:24:52 +0700

    Добавлены методы shuffleCells и getCellValue в класс Grid. Тест на возврат значений в промежутке от 0 до 15 после перемешки пройден

--- App/Grid.py
+++ App/Grid.py
@@ -1,6 +1,13 @@
 class Grid(object):
     #todo реализовать класс игровой сетки
-    pass
+    
+    def getCellValue(self, x, y):
+        #todo реализовать поиск €чейки и возврат еЄ значени€
+        return 0
+
+    def shuffleCells(self):
+        #todo реализовать перемешивание €чеек
+        pass
 
 
 
--- Tests/TestGrid.py
+++ Tests/TestGrid.py
@@ -6,3 +6,11 @@ class TestGrid(unittest.TestCase):
     def testGridClassCreation(self):
         grid = Grid()
         self.assertIsNotNone(grid)
+
+    def testGridShuffleCellsReturnValueInInterval(self):
+        grid = Grid()
+        grid.shuffleCells()
+        
+        for i in range(16):
+            cellValue = grid.getCellValue(i / 4, i % 4)
+            self.assertIs(0 <= cellValue <= 15, True)
